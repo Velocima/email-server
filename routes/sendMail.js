59ec2a33141c6e84ef67b9d4785db3bfffa5b27f
Velo <hartley.max@outlook.com>
2020-09-16 14:00:58 +0100

    code cleaned and error handling added

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -3,12 +3,6 @@ const nodemailer = require('nodemailer');
 const config = require('config');
 const validator = require('express-validator');
 
-// request object fields: 
-// name, subject, email, message.
-
-
-
-
 const sendMailRouter = express.Router();
 
 sendMailRouter.get('/sendmail', (req, res) => {
@@ -23,38 +17,33 @@ sendMailRouter.post('/sendmail/:sendTo',
     const { name, subject, email, message } = req.body.email;
 
     const mail = {
-        from: email,
+        from: config.get(`${sendToAddress}.email`), 
         to: config.get(`${sendToAddress}.email`),
         subject: subject,
-        html: `<p>Client name: ${name}. <br></br> Message: ${message}<p>`,
+        html: `<p>Client name: ${name}. <br></br>Client email ${email}.<br></br> Message: ${message}.<p>`,
     };
 
-    let transporter = nodemailer.createTransport({
-        host: config.get(`${sendToAddress}.host`),
-        port: config.get(`${sendToAddress}.port`),
-        secure: false,   
-        auth: {
-          user: config.get(`${sendToAddress}.email`),
-          pass: config.get(`${sendToAddress}.password`)
-        }
-    });
-    transporter.verify(function(error, success) {
-        if (error) {
-            console.log("Transporter is incorrectly configured.")
-            console.log(error);
-        } else {
-            console.log("Server is ready to take our messages");
-        }
-    });
+    try {
+        let transporter = nodemailer.createTransport({
+            host: config.get(`${sendToAddress}.host`),
+            port: config.get(`${sendToAddress}.port`),
+            secure: false,   
+            auth: {
+                user: config.get(`${sendToAddress}.email`),
+                pass: config.get(`${sendToAddress}.password`)
+            }
+        });
 
-    transporter.sendMail(mail, (err) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send();
-            return;
-        }
-        res.send('mail sent');
-    })
+        transporter.sendMail(mail, (err) => {
+            if (err) {
+                throw err;
+            }
+            res.send('mail sent');
+        });
+    } catch(err) {
+        console.error(err);
+        res.status(500).send();
+    }
 });
 
 module.exports = sendMailRouter;
