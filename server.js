4ab5409222a3828844c1e20f2794cf00182fd4ce
Velo <hartley.max@outlook.com>
2020-10-18 23:32:42 +0100

    cors added. ip whitelist removed

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const config = require('config');
+const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require("express-rate-limit");
-const ipfilter = require('express-ipfilter').IpFilter
 
 const app = express();
 app.use(express.static('public/index.html'));
 
 // Init middleware
-const ips = config.get("ipWhitelist");
-app.use(ipfilter(ips, { mode: 'allow' }))
+
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -18,6 +17,7 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 app.use(morgan('short'));
+app.use(cors({origin: "*"}));
 app.use(helmet());
 app.use(express.json());
 
@@ -25,9 +25,3 @@ app.use('/api/', require('./routes/sendMail'))
 
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT);
-
-console.log(`Server is running on port ${PORT}`);
-
-app.get('/api', (req, res) => res.send("(ﾉ◕ヮ◕)ﾉ*:･ﾟ"));
